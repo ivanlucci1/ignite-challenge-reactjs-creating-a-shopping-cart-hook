5f16bd380c048d3a1ea812e807666c880a10e279
Ivan Lucci <ivan@hagens.com.br>
2022-02-12 18:12:17 -0300

    [change] added use of Stock type on useCart

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -37,7 +37,7 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
       const updatedCart = [...cart];
       const productExists = updatedCart.find(product => product.id === productId);
 
-      const stock = await api.get(`/stock/${productId}`);
+      const stock = await api.get<Stock>(`/stock/${productId}`);
 
       const stockAmount = stock.data.amount;
       const currentAmount = productExists ? productExists.amount : 0;
@@ -89,7 +89,7 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
       const updatedCart = [...cart];
       const productIndex = updatedCart.findIndex(product => product.id === productId);
 
-      const stock = await api.get(`/stock/${productId}`);
+      const stock = await api.get<Stock>(`/stock/${productId}`);
       const stockAmount = stock.data.amount;
 
       if (stockAmount >= amount) {
